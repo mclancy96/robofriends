692816eb22519395f4bdbc723947aa2c005a5bbf
Mike <mclancy96@gmail.com>
2021-05-09 19:24:32 -0400

    Adding cards

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+cosnt Card = () => {
+    return (
+        <div>
+            <img src="https://robohash.org/test?200x200" alt="robots"></img>
+            <div>
+                <h2>Jane Doe</h2>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Card;
