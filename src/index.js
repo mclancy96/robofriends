e5c90d21b43609c8b6b642332199bb9dc7880752
Mike <mclancy96@gmail.com>
2021-05-09 19:15:09 -0400

    deleting files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Hello greeting={"Hello"}/>,
   document.getElementById('root')
 );
 
