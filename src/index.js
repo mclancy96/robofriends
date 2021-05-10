69e77ffb2f8c588f3afc931c6417e8e376cdb18c
Mike <mclancy96@gmail.com>
2021-05-09 20:10:01 -0400

    Adding app.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import CardList from './CardList';
 import 'tachyons';
 import {robots} from './robots'
+import App from './App.js'
 
-ReactDOM.render(
-    <CardList robots={robots}/>
- , document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 
