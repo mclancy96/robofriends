e8a25ae7e227bf5c2ff8e27cfdee267be58d4710
Mike <mclancy96@gmail.com>
2021-05-09 20:02:20 -0400

    Adding cardlist component

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2'>
-            <img src="https://robohash.org/test?200x200" alt="robots"></img>
+        <div className='bg-light-green dib br3 pa3 ma2 tc'>
+            <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"></img>
             <div>
-                <h2>Jane Doe</h2>
-                <p>janedoe@email.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import 'tachyons';
 import {robots} from './robots'
 
 ReactDOM.render(
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    </div>
+    <CardList robots={robots}/>
  , document.getElementById('root')
 );
 
