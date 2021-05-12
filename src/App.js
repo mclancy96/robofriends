fb89b86975b6e2701356037a6417dd89eef9fefe
Mike <mclancy96@gmail.com>
2021-05-11 21:29:14 -0400

    Adding api fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -10,9 +9,15 @@ class App extends Component {
    constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
        }
+   }
+
+   componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}))
    }
    onSearchChange = (event) => {
         this.setState({searchfield: event.target.value});
@@ -21,14 +26,18 @@ class App extends Component {
          const filteredRobots = this.state.robots.filter(robots =>{
               return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
          })
-       return (
-            <div className="tc">
-                <h1 className="coolFont">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-            
-        )
+         if(this.state.robots.length === 0){
+              return <h1>Loading...</h1>
+         } else {
+               return (
+                    <div className="tc">
+                         <h1 className="coolFont">RoboFriends</h1>
+                         <SearchBox searchChange={this.onSearchChange}/>
+                         <CardList robots={filteredRobots}/>
+                    </div>
+               )
+         }
+       
    }
 }
 
