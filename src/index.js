14baf0cbfbcdeefdcc79e234c8d910f203e4597d
Mike <mclancy96@gmail.com>
2021-05-11 21:49:12 -0400

    Rearranged files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
 import 'tachyons';
-import {robots} from './robots'
-import App from './App.js'
+import App from './containers/App.js'
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
