148e3082b3f00f3b7a3223da2927d000c5733e1b
Mike <mclancy96@gmail.com>
2021-05-11 21:37:28 -0400

    Modifying scroll file

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    return props.children;
+    return (
+        <div style={{overflowY:'scroll', border:'1px solid black', height: '1000px'}}>
+            {props.children}
+        </div>
+    )
 }
 
 export default Scroll;
